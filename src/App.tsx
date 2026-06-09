8c171d0f7b13f114b827f04f95a4e34d84d7129d
RUNWAT <sevenstar2202@gmail.com>
2026-06-09 14:05:51 +0600

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import { useState } from 'react'
-import { signUp, signIn, signOut } from './auth'
-
 function App() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
+  
   return (
-    <div style={{ padding: 20 }}>
-      <h2>Auth Demo</h2>
-
-      <input
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br />
-
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br /><br />
-
-      <button onClick={() => signUp(email, password)}>
-        Sign Up
-      </button>
-
-      <button onClick={() => signIn(email, password)}>
-        Sign In
-      </button>
-
-      <button onClick={() => signOut()}>
-        Sign Out
-      </button>
-    </div>
+    <>
+      <p>ok</p>
+    </>
   )
 }
 
